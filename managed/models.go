f5e3f9b8b054a7b5d19d002c78e53e7f7b2df3d5
Danish Mujeeb <danish.mujeeb@gmail.com>
2024-11-19 01:37:25 +0000

    added json field namesX

--- managed/models.go
+++ managed/models.go
@@ -5,11 +5,11 @@ import (
 )
 
 type BloodGlucoseReading struct {
-	Value     int64
-	Timestamp time.Time
+	Value     int64     `json:"value"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 type BloodGlucoseGraph struct {
-	PatientID string
-	Values    []BloodGlucoseReading
+	PatientID string                `json:"patient_id"`
+	Values    []BloodGlucoseReading `json:"values"`
 }
